7cae62c1e90e458bb954c44605513d3aec5d01ce
wmoralesdev <walterrafael26@gmail.com>
2020-10-15 11:41:24 -0600

    Initialize directory and index

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const http = require('http')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || '3000'
+app.set('port', port)
+
+var server = http.createServer(app)
+server.listen(port)
+
+server.on('error', () => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+})
+
+server.on('listening', () => {
+    console.log(`Listening on ${port}`);
+})
